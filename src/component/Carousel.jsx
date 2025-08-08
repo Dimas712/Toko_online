fe00ac11a5204d68c8ba3ef31bf804cef164053c
Dimas712 <jasinga1933@gmail.com>
2025-08-08 16:13:32 +0700

    update

--- src/component/Carousel.jsx
+++ src/component/Carousel.jsx
@@ -1,10 +1,11 @@
-// components/SKCarousel.jsx
+// components/SKCarousel.tsx
 "use client";
 
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/free-mode";
-import { FreeMode, Autoplay } from "swiper/modules";
+import "swiper/css/scrollbar";
+import { FreeMode, Scrollbar, Autoplay } from "swiper/modules";
 
 const dataSK = [
   "Makalah",
@@ -19,51 +20,33 @@ const dataSK = [
   "Pembuatan Website",
 ];
 
-// Bagi dua bagian
-const half = Math.ceil(dataSK.length / 2);
-const topRow = [...dataSK.slice(0, half)];
-const bottomRow = [...dataSK.slice(half)];
-
-// Duplikasikan item agar looping smooth
-const duplicateItems = (items) => [...items, ...items];
-
-const CarouselRow = ({ items, reverse = false }) => (
-  <Swiper
-    slidesPerView={"auto"}
-    spaceBetween={16}
-    freeMode={true}
-    loop={true}
-    speed={6000}
-    autoplay={{
-      delay: 0,
-      disableOnInteraction: false,
-      reverseDirection: reverse,
-    }}
-    modules={[FreeMode, Autoplay]}
-    allowTouchMove={false}
-    className="w-full"
-  >
-    {duplicateItems(items).map((item, idx) => (
-      <SwiperSlide key={idx} className="!w-fit">
-        <div className="bg-gray-700 text-white rounded-full px-6 py-3 font-semibold text-center shadow whitespace-nowrap">
-          {item}
-        </div>
-      </SwiperSlide>
-    ))}
-  </Swiper>
-);
-
-
 export default function Carousel() {
   return (
-    <div className="w-full px-4 py-6 space-y-4">
-      {/* Atas: ke kiri */}
-      <CarouselRow items={topRow} reverse={true} />
-
-      {/* Bawah: ke kanan */}
-      <CarouselRow items={bottomRow} reverse={false} />
+    <div className="w-full px-4 py-6">
+      <Swiper
+        slidesPerView={"auto"}
+        spaceBetween={16}
+        freeMode={true}
+        loop={true}
+        speed={3000}
+        autoplay={{
+          delay: 0,
+          disableOnInteraction: false,
+        }}
+        modules={[FreeMode, Scrollbar, Autoplay]}
+        className="mySwiper"
+      >
+        {dataSK.map((item, idx) => (
+          <SwiperSlide
+            key={idx}
+            className="!w-fit"
+          >
+            <div className="bg-gray-700 text-white rounded-full px-6 py-3 font-semibold text-center shadow whitespace-nowrap">
+              {item}
+            </div>
+          </SwiperSlide>
+        ))}
+      </Swiper>
     </div>
   );
 }
-
-
